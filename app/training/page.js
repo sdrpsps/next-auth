3a2743b47639bc18d77d39dc0158803ee144e4c9
sdrpsps <z826700@live.cn>
2024-09-20 17:04:16 +0800

    protect auth route

--- app/training/page.js
+++ app/training/page.js
@@ -1,6 +1,14 @@
-import { getTrainings } from '@/lib/training';
+import { verifyAuthSession } from "@/lib/auth";
+import { getTrainings } from "@/lib/training";
+import { redirect } from "next/navigation";
 
 export default async function TrainingPage() {
+  const { user, session } = await verifyAuthSession();
+
+  if (!user || !session) {
+    return redirect("/");
+  }
+
   const trainingSessions = getTrainings();
 
   return (
